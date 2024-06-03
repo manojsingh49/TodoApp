8acf19ed90d1f15963cec4903ae7502c808fc0e8
Anan Moktan <ananmoktan41@gmail.com>
2024-06-03 11:13:27 -0500

    css improved

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,55 +1,60 @@
 import React, { useEffect, useState } from "react";
+ import "./Todo.css"
 const TodoApp = () => {
-  const [tasks, setTasks] = useState([]); //Use the 'useState' hook to create two state varible. To stoe the list of task and newTask (To )
+  const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
-
+ 
   useEffect(() => {
-    fetch("https://jasonplaceholder.typecode.com/todos") // Fetching Data: Use Effect to fetch task from the API.
-      .then((response) => response.jason())
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
       .then((data) => setTasks(data));
-  });
+  }, []);
+ 
+  const addTask = () => {
+    const newTaskObj = {
+      title: newTask,
+      completed: false,
+      id: tasks.length + 1,
+    };
+    setTasks([...tasks, newTaskObj]);
+    setNewTask("");
+  };
+ 
+  const updateTask = (id, updatedTask) => {
+    setTasks(tasks.map((task) => (task.id === id ? updatedTask : task)));
+  };
+ 
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+ 
+  return (
+    <div>
+      <h1>Task List</h1>
+      <input
+        type="text"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+        placeholder="Add new Task"
+      />
+      <button onClick={addTask}>Add Task</button>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <input
+              type="checkbox"
+              checked={task.completed}
+              onChange={() =>
+                updateTask(task.id, { ...task, completed: !task.completed })
+              }
+            />
+            {task.title}
+            <button onClick={() => deleteTask(task.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
-const addTask = () => {
-  // Add task function(To create a new task object, add it to the 'tasks'array, and rest the 'newTask'input)
-  const newTaskObj = { title: newTask, completed: false };
-  setTasks([tasks, newTaskObj]);
-  setNewTask("");
-};
-const updateTask = (id, updateTask) => {
-  // Update function(To update task by its ID)
-  setTasks(tasks.map((task) => task, id == id ? updateTask : task));
-};
-const deleteTask = (id) => {
-  // Delete Function(To delete task by its ID)
-  setTask(tasks.filter((task) => task.id !== id));
-};
-return (
-  // Render Function (It returns a title, an input field for adding task and list of tasks.)
-  <div>
-    className ="todo-app
-    <h1>Task List</h1>
-    <input
-      type="text"
-      value={newTask}
-      onChange={(e) => setNewTaske(e.target.value)}
-      placeholder="Add new Task"
-    />
-    <button onClick={addTask}> Add Task</button>
-    <ul>
-      {task.map((task) => (
-        <li key={task.id}>
-          <input
-            type="checkbox"
-            checked={task.completed}
-            onchange={() =>
-              updateTask(task.id, { ...task, completed: !task.completed })
-            }
-          />
-          {task.title}
-          <button onClick={() => deleteTask(task.id)}>Delete</button>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+ 
 export default TodoApp;
